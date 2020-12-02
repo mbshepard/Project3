18400b4b4a37ad44b5a93f89748ad0354498b110
Gshow <chukwuanumba@gmail.com>
2020-12-01 22:07:22 -0800

    heroku test

--- heroku-test-deploy/audioList.js
+++ heroku-test-deploy/audioList.js
@@ -2,18 +2,18 @@ const AudioList=[
     {
       title: "American Dreamin",
       artists: "JayZ",
-      path: "/Users/Gshow/Desktop/Songs for App/03 American Dreamin'.mp3"
+      path: "/Users/ralphanumba/Desktop/songs for app/03 American Dreamin'.mp3"
       
     },
     {
       title: "Backseat Freestyle",
       artists: "Kendrick Lamar",
-      path: "/Users/Gshow/Desktop/Songs for App/03 Backseat Freestyle.mp3"
+      path: "/Users/ralphanumba/Desktop/songs for app/03 Backseat Freestyle.mp3"
     },
     {
       title: "Nice Watch",
       artists: "JCole",
-      path: "/Users/Gshow/Desktop/Songs for App/07 Mr. Nice Watch [Explicit Version].mp3"
+      path: "/Users/ralphanumba/Desktop/songs for app/07 Mr. Nice Watch [Explicit Version].mp3"
     }
     
   
